97ff4a84ef8107525736afb7b6e937d215acd851
NghiNV <nghinv03@gmail.com>
2024-10-11 16:01:23 +0700

    Doctor and patient endpoint

--- NWEB01.Application/DTOs/UserDTO.cs
+++ NWEB01.Application/DTOs/UserDTO.cs
@@ -12,7 +12,6 @@ namespace NWEB01.Application.DTOs
 		public string Name { get; set; }
 		public string Email { get; set; }
 		public DateTime? DateOfBirth { get; set; }
-		public string Password { get; set; }
 		public int Role { get; set; }
 		public string Specialization { get; set; }
 	}
