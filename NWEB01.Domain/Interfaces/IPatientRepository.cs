8400c130afa6e4b691bb5f4fae7578f80a8f0c64
NghiNV <nghinv03@gmail.com>
2024-10-11 23:39:47 +0700

    PatientRepository

---
+++ NWEB01.Domain/Interfaces/IPatientRepository.cs
@@ -0,0 +1,13 @@
+﻿using NWEB01.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWEB01.Domain.Interfaces
+{
+	public interface IPatientRepository : IBaseRepository<User, Guid>
+	{
+	}
+}
---
+++ NWEB01.Repository/Repositories/PatientRepository.cs
@@ -0,0 +1,13 @@
+﻿using NWEB01.Domain.Entities;
+using NWEB01.Domain.Interfaces;
+using NWEB01.Repository.Data;
+
+namespace NWEB01.Repository.Repositories
+{
+	public class PatientRepository : BaseRepository<User, Guid>, IPatientRepository
+	{
+		public PatientRepository(AppDbContext dbContext) : base(dbContext)
+		{
+		}
+	}
+}
